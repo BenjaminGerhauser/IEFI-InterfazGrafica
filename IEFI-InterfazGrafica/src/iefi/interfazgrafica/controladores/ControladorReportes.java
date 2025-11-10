f4d595e4ce6e14b464a2d523824753bf6e2394f8
JuaniPeriotti <periottijuani@gmail.com>
2025-11-10 01:04:24 -0300

    .

---
+++ IEFI-InterfazGrafica/src/iefi/interfazgrafica/controladores/ControladorReportes.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package iefi.interfazgrafica.controladores;
+
+/**
+ *
+ * @author Juani
+ */
+public class ControladorReportes {
+    
+}
